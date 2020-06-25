67c2ac78c3125649b0211a9199fd27bb8806935b
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-06-25 14:50:30 -0400

    added some pseudocode

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+// require fs
+// require inquirer
+
 // array of questions for user
 const questions = [
-
+    // https://www.npmjs.com/package/inquirer#questions
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    // Use the fs package
 }
 
 // function to initialize program
 function init() {
-
+    // Use the inquirer package
 }
 
 // function call to initialize program
